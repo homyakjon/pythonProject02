859a377c896aceb2e21a36837b43c462363382c0
Yurii <you@example.com>
2022-09-28 18:28:51 +0300

    My new project

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from datetime import datetime
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Developer:
+    def __init__(self, name, dob, employment_date, unit, profile, salary, work_day):
+        self.name = name
+        self.dob = self.transform_date(dob)
+        self.employment_date = self.transform_date(employment_date)
+        self.unit = unit
+        self.profile = profile
+        self.salary = salary
+        self.work_day = work_day
+
+    def transform_date(self, date_as_string: str) -> datetime:
+        date_as_object = datetime.strptime(date_as_string, "%d.%m.%Y")
+        return date_as_object
+
+    def check_work_experience(self):
+        today = datetime.today()
+        experience = today - self.employment_date
+        return experience
+
+    def increase_salary(self, amount, restrictions=True):
+        if amount == self.salary * 0.1 and restrictions:
+            print("You can increase salary more than 10%")
+            self.salary += amount
+            print(f"msg: new salary is {self.salary}")
+        else:
+            print(f"Not can increase salary")
+
+    def data_processing(self):
+        if self.check_work_experience() >= 24:
+            self.increase_salary(
+                self.salary * 0.3,
+                restrictions=False
+            )
+
+    def work_bonus(self, bonus=300):
+        if self.work_day > 8:
+            print(f"add bonus developer in size {bonus} dollars")
+            self.salary += bonus
+        else:
+            print("Sorry, developer not get bonus")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Junior(Developer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Senior(Developer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+junior = Junior(
+    name="Stan",
+    dob="11.05.1990",
+    employment_date="07.04.2021",
+    unit="B",
+    profile="Python",
+    salary=1000,
+    work_day=10
+
+)
+
+
+senior = Senior(
+    name="Billy",
+    dob="08.11.1989",
+    employment_date="05.02.2018",
+    unit="B",
+    profile="Python",
+    salary=4000,
+    work_day=10
+
+)
+
